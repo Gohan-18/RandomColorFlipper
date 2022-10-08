bb6cac4049a0fa1a869106cb0533aacf334acdb5
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2022-10-08 22:26:42 +0530

    RESET testing

--- simple.js
+++ simple.js
@@ -2,14 +2,23 @@ const colors = ['red', 'green', 'blue', 'yellow', 'grey', 'pink', 'cyan', 'orang
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+// const header = document.querySelector("h4");
 
 
-btn.addEventListener("click", function() {
+btn.addEventListener("click", () => {
     const randomNumber = getRandomNumber();
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 })
+
+// header.addEventListener("mouseover", () => {
+//     header.textContent = "----- RESET -----";
+// })
+
+// header.addEventListener("mouseout", () => {
+//     header.textContent = "COLOR FLIPPER";
+// })
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
